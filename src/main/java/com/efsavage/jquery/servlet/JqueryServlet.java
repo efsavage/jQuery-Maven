1f0dcc434e2fc8d179d7cf85cf45056b46edefcb
Eric Savage <code@efsavage.com>
2011-10-10 15:15:52 -0400

    Create project, version 1.6.4

---
+++ src/main/java/com/efsavage/jquery/servlet/JqueryServlet.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2011 Eric F. Savage, code@efsavage.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.efsavage.jquery.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Returns the jQuery core file. Add "min=false" parameter to get the full
+ * non-minified version.
+ * 
+ * @author <a href="http://efsavage.com">Eric F. Savage</a>, <a
+ *         href="mailto:code@efsavage.com">code@efsavage.com</a>.
+ * 
+ */
+@WebServlet(urlPatterns = "/ext/js/jquery.js")
+public class JqueryServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/javascript");
+		PrintWriter out = resp.getWriter();
+		String src = "/com/jquery/jquery-1.6.4.min.js";
+		if ("false".equals(req.getParameter("min"))) {
+			src = "/com/jquery/jquery-1.6.4.js";
+		}
+		InputStream jsStream = getClass().getResourceAsStream(src);
+		String js = new Scanner(jsStream).useDelimiter("\\A").next();
+		out.write(js);
+	}
+}
